e9809ce335b11ccea135ef81b7cefc3ce9266e1f
Derrick Mulinde <djmulinde@gmail.com>
2023-10-23 11:01:36 +0300

    Add products endpoints

--- src/main/java/com/backend/agriculturalsupportapp/controller/ProductController.java
+++ src/main/java/com/backend/agriculturalsupportapp/controller/ProductController.java
@@ -1,2 +1,39 @@
-package com.backend.agriculturalsupportapp.controller;public class ProductController {
+package com.backend.agriculturalsupportapp.controller;
+
+import com.backend.agriculturalsupportapp.model.Product;
+import com.backend.agriculturalsupportapp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Product>> getProductsByCategory(@PathVariable String category) {
+        List<Product> products = productService.getProductsByCategory(category);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
