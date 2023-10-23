59e154c512252bebe56ae56a13893528ce18ba57
Derrick Mulinde <djmulinde@gmail.com>
2023-10-23 11:00:59 +0300

    Add products repository

--- src/main/java/com/backend/agriculturalsupportapp/repository/ProductRepository.java
+++ src/main/java/com/backend/agriculturalsupportapp/repository/ProductRepository.java
@@ -1,2 +1,14 @@
-package com.backend.agriculturalsupportapp.repository;public interface ProductRepository {
+package com.backend.agriculturalsupportapp.repository;
+
+import com.backend.agriculturalsupportapp.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(String category);
+    Optional<Product> findById(Integer id);
 }
