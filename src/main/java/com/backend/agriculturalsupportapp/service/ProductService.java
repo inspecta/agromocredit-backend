a77ac0bf466a741c8b4cb6f329908e814ae75510
Derrick Mulinde <djmulinde@gmail.com>
2023-10-23 11:01:17 +0300

    Add products service

--- src/main/java/com/backend/agriculturalsupportapp/service/ProductService.java
+++ src/main/java/com/backend/agriculturalsupportapp/service/ProductService.java
@@ -1,2 +1,26 @@
-package com.backend.agriculturalsupportapp.service;public class ProductService {
+package com.backend.agriculturalsupportapp.service;
+
+import com.backend.agriculturalsupportapp.model.Product;
+import com.backend.agriculturalsupportapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
 }
